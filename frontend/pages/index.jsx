807d9f00ab40c670a6f1f332a9e1e1dcaec0fd29
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-03-16 19:09:08 +0400

    setup styles

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -3,7 +3,10 @@ import { Layout } from "../components";
 const Index = () => {
   return (
     <Layout>
-      <h2>Index page</h2>
+      <h2 className="main__title">Index page</h2>
+      <div className="custom">
+        <p className="text">next</p>
+      </div>
     </Layout>
   );
 };
